a49d73f36c71aa9c0d0d26995113d7b31bf5e157
catalog-tomatov <korolev94sergey@gmail.com>
2026-09-24 21:48:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "catalog-shell-v132";
+const SHELL_CACHE = "catalog-shell-v133";
 const IMAGE_CACHE = "catalog-images-v5";
 const BADGE_COUNTER_URL = "./__catalog_badge_counter__";
 const SHELL_FILES = [
@@ -6,7 +6,7 @@ const SHELL_FILES = [
   "./index.html",
   "./style.css?v=73",
   "./script.js?v=92",
-  "./chat.js?v=20260924-sync9",
+  "./chat.js?v=20260924-sync10",
   "./firebase-config.js?v=1",
   "./firebase-client.js?v=20260913-sync2",
   "./manifest.json",
